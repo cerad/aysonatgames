99c73dd6781b5aa65c8c5b6b63a85f6a24e65ddb
Art Hundiak <ahundiak@gmail.com>
2013-07-06 08:59:21 -0500

    Override AYSO/RefereeBadgeFormType

---
+++ src/Cerad/Bundle/AppBundle/Form/Type/AYSO/RefereeBadgeFormType.php
@@ -0,0 +1,25 @@
+<?php
+namespace Cerad\Bundle\AppBundle\Form\Type\AYSO;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Cerad\Bundle\PersonBundle\Form\Type\AYSO\RefereeBadgeFormType as BaseFormType;
+
+class RefereeBadgeFormType extends BaseFormType
+{    
+    protected $refereeBadgeChoices = array
+    (
+        'None'         => 'None',
+        'Regional'     => 'Regional',
+        'Intermediate' => 'Intermediate',
+        'Advanced'     => 'Advanced',
+        'National'     => 'National',
+        'National1'    => 'National 1',
+        'National2'    => 'National 2',
+    );
+}
+
+?>
