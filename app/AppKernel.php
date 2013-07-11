af531b3e982ed58b4efca169db1ee983acb0de97
Art Willow Hundiak <ahundiak@zayso.org>
2013-07-11 15:11:55 -0500

    accounts v2 initial commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             
-            new FOS\UserBundle\FOSUserBundle(),
+          //new FOS\UserBundle\FOSUserBundle(),
             
             new Cerad\Bundle\Legacy2012Bundle\CeradLegacy2012Bundle(),
             
