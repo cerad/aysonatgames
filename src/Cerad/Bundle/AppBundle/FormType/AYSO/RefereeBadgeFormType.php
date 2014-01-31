7f68093fb87f8c68f5b8a271c97a0f28e2a02687
ahundiak <ahundiak@spike.mshome.net>
2014-01-31 11:08:43 -0600

    Updated PersonExportXLS for new schema format Updated RefereeBadgeForm

--- src/Cerad/Bundle/AppBundle/FormType/AYSO/RefereeBadgeFormType.php
+++ src/Cerad/Bundle/AppBundle/FormType/AYSO/RefereeBadgeFormType.php
@@ -3,10 +3,6 @@ namespace Cerad\Bundle\AppBundle\FormType\AYSO;
 
 use Cerad\Bundle\PersonBundle\FormType\AYSO\RefereeBadgeFormType as RefereeBadgeFormTypeBase;
 
-/* ==================================================================
- * Use this to collect and partially validate a region number
- * The transformer will yield AYSORxxxx
- */
 class RefereeBadgeFormType extends RefereeBadgeFormTypeBase
 {   
     protected $refereeBadgeChoices = array
