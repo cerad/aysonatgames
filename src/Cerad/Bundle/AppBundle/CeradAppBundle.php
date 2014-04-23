abc0d7c8484a06a46d14c229fcb33d18c95be2f0
ahundiak <ahundiak@spike.mshome.net>
2014-04-23 09:31:55 -0500

    Fixed path to favicon Tweaked CeradAppExtension

--- src/Cerad/Bundle/AppBundle/CeradAppBundle.php
+++ src/Cerad/Bundle/AppBundle/CeradAppBundle.php
@@ -3,13 +3,7 @@ namespace Cerad\Bundle\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-use Cerad\Bundle\AppBundle\DependencyInjection\AppExtension;
-
 class CeradAppBundle extends Bundle
 {  
-    public function getContainerExtension()
-    {
-        return new AppExtension();
-    }
 }   
 ?>
