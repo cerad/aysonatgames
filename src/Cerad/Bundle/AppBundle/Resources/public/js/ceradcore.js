19350866b3c4bcc287814e5fdcc8aad544307f98
ahundiak <ahundiak@giles>
2014-05-31 12:44:27 -0500

    Updated from jquery 1.8 to 1.10. Fixed ceradcore.js (now uses prop instead of attr)

--- src/Cerad/Bundle/AppBundle/Resources/public/js/ceradcore.js
+++ src/Cerad/Bundle/AppBundle/Resources/public/js/ceradcore.js
@@ -10,71 +10,38 @@ Cerad.alert = function(msg)
 /* =====================================================
  * This turns all the checkboxes in a given named group
  * On or Off
+ * 31 May 2014
+ * 
+ * This was developed under jquery 1.8.3
+ * I used attr('checked') to determine if the checkbox was checked
+ * Turns out that this was changed way back in 1.6.
+ * prop shuld be used.
+ * 
+ * This function stopped working when upgrading to jquery 1.10
+ * Changed attr to prop and all was well
+ * 
+ * Also at one point had names like refSchedSearchData[ages][All]
+ * Now we get form[ages][]
+ * Not sure if having elements with the same name is good.
+ * Need to look at
  */
 Cerad.checkboxAll = function(e)
-{
-    var nameRoot = $(this).attr('name'); // "refSchedSearchData[ages][All]";
+{   
+    var nameRoot = $(this).attr('name'); // "refSchedSearchData[ages][]";
         
     nameRoot = nameRoot.substring(0,nameRoot.lastIndexOf('['));
     
     var group = 'input[type=checkbox][name^="' + nameRoot + '"]';
     
     // attr return undefined if not set, 'checked' if it is
-    var checked = $(this).attr('checked') ? true : false;
+    var checked = $(this).prop('checked') ? true : false;
         
     $(group).attr('checked', checked);
 };
-/* ====================================================
- * A method for translating year/nomth/day checkboxes
- * Into a readable date string
- * I think it has been replaced with datepicker
+/* ==================================
+ * Used to have some date processing here
+ * Not used anymore
+ * See the s1games20131203 app for the code
  */
-Cerad.dateGen = function(e)
-{   
-    var nameChanged = $(this).attr('name'); // "refSchedSearchData[date1][month]";
-        
-    var nameRoot = nameChanged.substring(0,nameChanged.lastIndexOf('['));
-    
-    var year  = $('select[name="' + nameRoot + '[year]"]').val();
-    var month = $('select[name="' + nameRoot + '[month]"]').val();
-    var day   = $('select[name="' + nameRoot + '[day]"]').val();
-    
-    var desc  = $('input[name="' + nameRoot + '[date]"]');
-   
-    var date  = new Date(year,month-1,day);
-    
-    // Replace using datepicker if decide to always include it
-    desc.val(date.toDateString('M d yy D'));
-    
-    //alert('Year ' + date);
-};
-/* ==================================================
- * This gets triggered (by datepicker) when the desc field has changed
- * this = desc field
- * Sets the year/month/day elements
- * 
- * Not real pretty but seems to get the job done
- */
-Cerad.dateChanged = function(dateText) // 20120109
-{
-    // Just in case
-    if (dateText.length !== 8) return;
-    
-    var nameChanged = $(this).attr('name'); // "refSchedSearchData[date1][month]";
-    
-    var nameRoot = nameChanged.substring(0,nameChanged.lastIndexOf('['));
-    
-    var day   = dateText.substring( 6, 8);
-    var year  = dateText.substring( 0, 4);
-    var month = dateText.substring( 4, 6);
-
-    // console.log(nameRoot + ' ' + dateText + ' ' + year);
-    
-    $('select[name="' + nameRoot +   '[day]"]').val(day);
-    $('select[name="' + nameRoot +  '[year]"]').val(year);
-    $('select[name="' + nameRoot + '[month]"]').val(month);
-    return;  
-};
-
 
 
